823e7c1dba155d356d571425eca7c44a251f9598
faisalcep <faisalcep@yahoo.com>
2020-08-25 17:07:42 -0400

    first Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import Header from './components/Header/Header';
+import Users from './components/Users/Users';
+import Cart from './components/Cart/Cart';
 import './App.css';
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const [cart, setCart] = useState([]);
+
+  const handleAdd = (user) => {
+    const newCart = [...cart, user];
+    setCart(newCart);
+  };
+
+  useEffect(() => {
+    fetch('https://randomuser.me/api/?results=15')
+      .then((res) => res.json())
+      .then((data) => setUsers(data.results))
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header></Header>
+      <div className='app-container'>
+        <div className='user-list'>
+          {users.map((user) => (
+            <Users user={user} handleAdd={handleAdd}></Users>
+          ))}
+        </div>
+        <div className='cart-area'>
+          <h2>Total Users: {users.length}</h2>
+          <h3>Users Added: {cart.length}</h3>
+          <Cart cart={cart}></Cart>
+        </div>
+      </div>
     </div>
   );
 }
