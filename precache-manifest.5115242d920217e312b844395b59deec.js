b80dd0452037ad2d45caba79b6d7885c21bfd5fc
faisalcep <faisalcep@yahoo.com>
2020-08-27 15:50:31 -0400

    Updates

---
+++ precache-manifest.5115242d920217e312b844395b59deec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd692bfee5fd6bad2e0c244b8bb7efb3",
+    "url": "/random-users-react/index.html"
+  },
+  {
+    "revision": "e3168c51f0bfc6a02b77",
+    "url": "/random-users-react/static/css/main.80b21fac.chunk.css"
+  },
+  {
+    "revision": "fe05064616242a024966",
+    "url": "/random-users-react/static/js/2.f7aaa5ef.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/random-users-react/static/js/2.f7aaa5ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3168c51f0bfc6a02b77",
+    "url": "/random-users-react/static/js/main.fcbcbabe.chunk.js"
+  },
+  {
+    "revision": "df857dcfc57ce6fcd6ab",
+    "url": "/random-users-react/static/js/runtime-main.f77403bd.js"
+  }
+]);
